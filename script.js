9d8741a629c0270acb6a645fd156c3330a46f7dd
ThunderKlappe <timschley@gmail.com>
2021-12-15 08:41:52 -0800

    Changed arrow functions to normal functions so they would be hoisted

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ do{
 
 
 //This Function plays through a game
-let playGame = () =>{
+function playGame() {
     //Prompt for either rock, paper, or scissors from user
     userChoice = getUserChoice();
     //Sanitize user selection
@@ -36,8 +36,7 @@ let playGame = () =>{
     //Randomly select rock, paper, or scissors for computer
     compChoice = getCompChoice(choices);
     //compare user's input to computer's selection
-
-    result = getResult(userChoice, compChoice)
+    result = getResult(userChoice, compChoice);
 
     //display winner
     console.log(result);
@@ -46,24 +45,24 @@ let playGame = () =>{
 }
 
 //This function asks the user for their choice and returns their choice
-let getUserChoice = () =>{
-    return prompt("Would you like rock, paper, or scissors?",);
+function getUserChoice() {
+    return prompt("Would you like rock, paper, or scissors?");
 }
 
 //This function takes in the function, sets the case to lower, and checks to see if the user chose "rock", "paper", or "scissors"
-let sanitize = choice =>{
+function sanitize(choice) {
     choice = choice.toLowerCase();
-    if(choice == !("rock"||"paper"||"scissors")){
+    if (choice == !("rock" || "paper" || "scissors")) {
         choice = "none";
     }
     return choice;
 
 }
 
-let check = choice =>{
-    while(choice == "none"){
+function check(choice) {
+    while (choice == "none") {
         //if not, ask again
-        console.log("I'm sorry, please enter 'Rock', 'Paper', or 'Scissors' to choose what you would like to use.")
+        console.log("I'm sorry, please enter 'Rock', 'Paper', or 'Scissors' to choose what you would like to use.");
         choice = getUserChoice();
         choice = sanitize(choice);
     }
@@ -71,51 +70,51 @@ let check = choice =>{
 }
 
 //This function randomly selects from an array and returns the contents of the index selected.
-let getCompChoice = choiceArray =>{
-    let indexSel = Math.floor((Math.random() * 3))
+function getCompChoice(choiceArray) {
+    let indexSel = Math.floor((Math.random() * 3));
     return choiceArray[indexSel];
 }
 
 //This function takes in the user choice and the computer choice and returns what the result is
-let getResult = (user, comp) => {
-    if(user == "rock"){
-        if(comp == "rock"){
+function getResult(user, comp) {
+    if (user == "rock") {
+        if (comp == "rock") {
             tieCount++;
             return "You picked rock and I picked rock. We tied!";
         }
-        else if(comp == "paper"){
+        else if (comp == "paper") {
             compWinCount++;
             return "You picked rock and I picked paper. I win!";
         }
-        else if(comp == "scissors"){
+        else if (comp == "scissors") {
             userWinCount++;
             return "You picked rock and I picked scissors. You win!";
         }
     }
-    else if(user == "paper"){
-        if(comp == "rock"){
+    else if (user == "paper") {
+        if (comp == "rock") {
             userWinCount++;
             return "You picked Paper and I picked Rock. You win!";
         }
-        else if(comp == "paper"){
+        else if (comp == "paper") {
             tieCount++;
             return "You picked Paper and I picked Paper. We tied!";
         }
-        else if(comp == "scissors"){
+        else if (comp == "scissors") {
             compWinCount++;
             return "You picked Paper and I picked Scissors. I won!";
         }
     }
-    else if(user == "scissors"){
-        if(comp == "rock"){
+    else if (user == "scissors") {
+        if (comp == "rock") {
             compWinCount++;
             return "You picked Scissors and I picked Rock. I won!";
         }
-        else if(comp == "paper"){
+        else if (comp == "paper") {
             userWinCount++;
             return "You picked Scissors and I picked Paper. You won!";
         }
-        else if(comp == "scissors"){
+        else if (comp == "scissors") {
             tieCount++;
             return "You picked Scissors and I picked Scissors. We tied!";
         }
